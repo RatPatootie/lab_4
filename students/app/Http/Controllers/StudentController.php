d291ea49ad05551f42e53f96165f7c81a3902523
Petro <129007162+RatPatootie@users.noreply.github.com>
2025-05-11 22:00:06 +0300

    all work exept api (post/put/delete)

--- students/app/Http/Controllers/StudentController.php
+++ students/app/Http/Controllers/StudentController.php
@@ -46,7 +46,9 @@ class StudentController extends Controller
     }
 
     public function update(Request $request, $id)
-    {
+    {   
+        
+
         $student = Student::find($id);
         
         if (!$student) {
--- students/routes/web.php
+++ students/routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('api/students')->group(function () {
+Route::prefix('students')->group(function () {
     Route::get('/', [StudentController::class, 'index']);
     Route::get('/{id}', [StudentController::class, 'show']);
     Route::post('/', [StudentController::class, 'store']);
